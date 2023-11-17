f2b0497fed2d231a8cd47d459f8c0533412640da
BurningSunset <slavasol2003@rambler.ru>
2023-11-17 16:00:14 +0300

    test commit

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -7,19 +7,9 @@ function App() {
   return (
     <div className={styles.app}>
       <header className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Скоро здесь будет первая страница нашего проекта!
         </p>
-        <a
-          className={styles.appLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
       </header>
     </div>
   );
