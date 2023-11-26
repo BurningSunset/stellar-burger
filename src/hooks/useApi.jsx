c12f287a6fbc459f683076617c80a4449cab9a7f
BurningSunset <slavasol2003@rambler.ru>
2023-11-26 19:49:53 +0300

    Удалил catch в useApi

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -5,26 +5,22 @@ const useApi = (apiUrl) => {
     if (response.ok) {
       return response.json()
     }
-      setError(`Error (handleResponse): ${response.status}`)
-      return Promise.reject(`Error (handleResponse): ${response.status}`)
+    setError(response.status)
   }
   const getIngredientList = async () => {
-    try {
-      const response = await fetch(apiUrl, {
-        method: 'GET'
-      });
-      const data = await handleResponse(response);
-      if (data && data.data) {
-        return data;
-      } else {
+    const response = await fetch(apiUrl, {
+      method: 'GET'
+    });
+    const data = await handleResponse(response);
+    if (data && data.data) {
+      return data;
+    } else {
         setError(`Error (getIngredientList): Invalid data structure`);
-        return Promise.reject(`Error (getIngredientList): Invalid data structure`);
-      }
-        } catch (error) {
-            setError(`Error (getIngredientList): ${error.message}`);
-            return Promise.reject(`Error (getIngredientList): ${error.message}`);
-        }
-          };
-      return { getIngredientList, error };
     }
+  };
+  return {
+    getIngredientList,
+    error
+  };
+}
 export default useApi;
